b1bffbde693daee77aed6a1baea1d7f9dc7bdfaf
Meshack <rutomeshack18@gmail.com>
2024-08-18 19:15:04 +0300

    Change to views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,8 @@ from typing import Any
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+
 # Create your views here.
 def list_book(request):
     books = Book.objects.all()
