d5c51a3786f7930534257260649d32a37ffcc35f
Meshack <rutomeshack18@gmail.com>
2024-08-20 23:15:59 +0300

    login and logout functions

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('books/', views.list_books, name='books'),
     path('Library/', views.LibraryDetailView.as_view(), name='Library'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name = 'registration/logout.html'), name='logout'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView 
 
 # Create your views here.
 def list_books(request):
@@ -19,3 +22,7 @@ class LibraryDetailView(DetailView):
         library = self.get_object()
         context ['books'] = library.books.all()
 
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
