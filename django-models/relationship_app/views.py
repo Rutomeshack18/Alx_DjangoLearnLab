f8ecdfb8f6957ec6cb9023f353cdefffe91601ac
Meshack <rutomeshack18@gmail.com>
2024-08-22 21:59:45 +0300

    Changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -13,16 +13,17 @@ from django.contrib.auth import login
 def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request, 'relationship_app/list_books.html', context)
+    return render(request, 'templates/list_books.html', context)
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'templates/library_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         library = self.get_object()
         context ['books'] = library.books.all()
+        return context
 
 class Register(CreateView):
     form_class = UserCreationForm
