83dabc989ee1d229270803f21a4c8eea7f032449
Meshack <rutomeshack18@gmail.com>
2024-08-23 22:08:49 +0300

    Other Changes

--- django-models/relationship_app/admin.py
+++ django-models/relationship_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book, Library, Librarian
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
