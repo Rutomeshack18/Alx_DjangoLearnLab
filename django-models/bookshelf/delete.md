e21ff305d91e7c87d119cb39ad3b5c25605842ec
Meshack <rutomeshack18@gmail.com>
2024-08-24 11:04:42 +0300

    Major changes

--- django-models/bookshelf/delete.md
+++
@@ -1,4 +0,0 @@
-from bookshelf.models import Book
-book = Book.objects.get(title='Nineteen Eighty-Four')
-book.delete()
-<!-- <QuerySet [<Book: Book object (1)>]> -->
--- django-models/relationship_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Book, Library, Librarian
-
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Library)
-admin.site.register(Librarian)
