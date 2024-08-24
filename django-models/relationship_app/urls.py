bd7e67476f144a78b0aa6afedc306106d9428e70
Meshack <rutomeshack18@gmail.com>
2024-08-24 15:40:11 +0300

    Role based access

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,6 +3,9 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
