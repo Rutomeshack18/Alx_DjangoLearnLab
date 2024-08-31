05e5976c25845dab4ab3740f83b67f6dd4e23079
Meshack <rutomeshack18@gmail.com>
2024-08-31 12:38:28 +0300

    Viewsets and Routers

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'Book', BookViewSet)
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-    # Add any additional non-viewset-based endpoints here
+    path('api/', include(router.urls)),
 ]
