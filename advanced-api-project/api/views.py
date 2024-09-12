89f0929e9428be43a57c7d72f987b8ec7ec6955f
Meshack <rutomeshack18@gmail.com>
2024-09-12 13:51:17 +0300

    Changes to the urls.py file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
 from . models import Book
 from .serializers import BookSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 # Create your views here.
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_class = [permissions.AllowAny]
+    permission_class = [IsAuthenticatedOrReadOnly]
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_class = [permissions.AllowAny]
+    permission_class = [IsAuthenticatedOrReadOnly]
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_class = [permissions.IsAuthenticated]
+    permission_class = [IsAuthenticated]
 
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_class = [permissions.IsAuthenticated]
+    permission_class = [IsAuthenticated]
 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_class = [permissions.IsAuthenticated]
+    permission_class = [IsAuthenticated]
