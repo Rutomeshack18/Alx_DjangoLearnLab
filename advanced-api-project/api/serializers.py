7f89e7e34c9324896e2613e02cee46cca9d2fa85
Meshack <rutomeshack18@gmail.com>
2024-09-12 07:14:44 +0300

    Minor change to serializers.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -17,7 +17,7 @@ class BookSerializer(serializers.ModelSerializer):
         return value
 
 class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many = True, read_only = True)
+    books = BookSerializer(many=True, read_only=True)
     class Meta:
         model = Author
         fields = ['name', 'books']
