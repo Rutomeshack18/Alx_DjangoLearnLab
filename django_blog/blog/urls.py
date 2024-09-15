a0161c9952c8e03939599ea0f2d3d7ba1c16db70
Meshack <rutomeshack18@gmail.com>
2024-09-15 21:49:51 +0300

    Changed the location of login and logout html templates

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,8 +4,8 @@ from . views import register
 from . views import  profile
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name = 'registration/login.html'), name = 'login'),
-    path('logout/', LogoutView.as_view(template_name = 'registration/logout.html'), name = 'logout'),
+    path('login/', LoginView.as_view(template_name = 'login.html'), name = 'login'),
+    path('logout/', LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),    
 ]
