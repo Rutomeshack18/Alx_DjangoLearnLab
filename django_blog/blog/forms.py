61859f07d7da0ed4190f488343fcac0371047150
Meshack <rutomeshack18@gmail.com>
2024-09-22 21:56:35 +0300

    Changes to forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text='Required. Enter a valid email address.')
 
@@ -15,7 +17,8 @@ class PostCreationForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
-        'content': forms.Textarea( attrs= {'rows': 10, 'cols': 80})
+        'content': forms.Textarea( attrs= {'rows': 10, 'cols': 80}),
+        'tags': TagWidget()
         }
 
     def clean_title(self):
