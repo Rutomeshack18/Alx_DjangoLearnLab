f66be345b6c5abed037a24dde4b9a5933de2fe3c
Meshack <rutomeshack18@gmail.com>
2024-10-01 05:01:54 +0300

    Added Follow and Unfollow functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import register, login
-
+from .views import follow_user, unfollow_user, FeedView
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
+    path('follow/<int:user_id>/', follow_user, name='follow_user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow_user'),
+    path('feed/', FeedView.as_view(), name='user_feed'),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,7 +4,11 @@ from rest_framework import status
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from posts.serializers import PostSerializer
+from rest_framework import generics
+from posts.models import Post
 @api_view(['POST'])
 def register(request):
     serializer = UserSerializer(data=request.data)
@@ -27,3 +31,35 @@ def login(request):
         token, created = Token.objects.get_or_create(user=user)  # Retrieve or create token
         return Response({'token': token.key}, status=status.HTTP_200_OK)
     return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+User = get_user_model()
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({"message": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = User.objects.get(id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({"message": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # Get the posts from users that the current user follows
+        following = user.following.all()
+        return Post.objects.filter(author__in=following).order_by('-created_at')
